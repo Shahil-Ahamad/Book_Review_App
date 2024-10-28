6fdeb720a1b4b7e9793880d8753db6e4b6579d29
Shahil-Ahamad <shahilahamad04@gmail.com>
2024-10-28 20:06:48 +0545

    initial

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,65 +1,57 @@
-import cookieParser from "cookie-parser";
 import express, { Request, Response, NextFunction } from "express";
+import cookieParser from "cookie-parser";
 import cors from "cors";
-
-const PORT = 4000;
-
-const app = express();
-
-app.use(express.json());
-app.use(cookieParser());
-
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-); 
+import { env } from "./utils/config";
+import { APIError } from "./utils/error";
+import { createDBConnection } from "./utils/db";
 
 
 createDBConnection()
-  .then(() => {
+.then(() => {
     console.log("Database connected successfully");
-  })
-  .catch((error) => {
-    console.error("Database connection error:", error);
-  });
+})
+.catch((error) => {
+    console.log("Database connection error:", error);
+});
+
+const app = express();
+
+app.use(
+    cors({
+        origin: "*",
+        credentials: true,
+    })
+);
+app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.json({
-    message: "Welcome To Book Review App",
-    data: null,
-    isSuccess: true,
-  });
+    res.json({
+        message: "Welcome to Book Review App",
+        data: null,
+        isSuccess: true,
+    });;
+})
+
+//Global Error Handler
+
+app.use((error: APIError, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+    if (error instanceof APIError) {
+        res.status(error.status).json({
+            message: error.message,
+            data: null,
+            isSuccess: false,
+        });
+        return;
+    }
+    res.status(500).json({
+        message: "Internal server error",
+        data: null,
+        isSuccess: false,
+    });
 });
 
-//Global Error Handlerer
-
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  console.error(err);
-  res.status(500).json({
-    message: "something went wrong",
-  });
-});
-
-
-// app.use((error: APIError, req: Request, res: Response, next: NextFunction) => {
-//   console.error(error);
-//   if (error instanceof APIError) {
-//     res.status(error.status).json({
-//       message: error.message,
-//       data: null,
-//       isSuccess: false,
-//     });
-//     return;
-//   }
-//   res.status(500).json({
-//     message: "Internal server error",
-//     data: null,
-//     isSuccess: false,
-//   });
-// });
-
-app.listen(env.PORT, () =>
-  console.log(`Server started on: http://localhost:${env.PORT}`)
+app.listen(env.PORT, () => 
+console.log(`Server started on: http://localhost:${env.PORT}`)
 );
